6611f037ae6db699da8b118ee7f49932227134ab
imamhsn195@gmail <imamhsn195@gmail.com>
2019-09-12 13:14:45 +0600

    lodash installed

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ console.log('Starting app.js');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
-console.log('Result:', notes.add(9, -5));
+const _ = require('lodash');
+
+var filteredArray = _.uniq(['imam', 2, 1, 2, "Imam"]);
+console.log(filteredArray);
+
+// console.log(_.isString(true));
+// console.log(_.isString("Imam"));
+// console.log('Result:', notes.add(9, -5));
+
 // var user = os.userInfo();
 // fs.appendFile('greeting.txt', `Hello ${user.username}! you are ${notes.age}`, (err) => {
 //     if (err) throw err;
